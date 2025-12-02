9ada62820ef6e866d6ebc79d7a43f7503450ed39
Ethan <ethan.umana2@gmail.com>
2025-12-02 15:13:27 -0500

    steps 5.1 and 5.2.1

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,20 @@
 import { getFirestore } from "firebase/firestore";
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export const firebaseConfig = {
-  // COPY this from your Firebase Console
-  apiKey: "your-api-key-goes-here",
-  authDomain: "your-project-name-here.firebaseapp.com",
-  databaseURL: "https://your-project-name-here.firebaseio.com",
-  projectId: "your-project-name-here",
-  storageBucket: "your-project-name.appspot.com",
-  messagingSenderId: "xxxxxxxx",
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJzeJYHqaa8mIv1MVXDiuhyqUhQPdc1o0",
+  authDomain: "cis371-a5bc3.firebaseapp.com",
+  projectId: "cis371-a5bc3",
+  storageBucket: "cis371-a5bc3.firebasestorage.app",
+  messagingSenderId: "504418340",
+  appId: "1:504418340:web:063c31389e70578320c8b1"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
